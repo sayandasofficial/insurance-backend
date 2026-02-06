a693ae2b126140ac44d4f775db41067996fc8170
Sayan Das <sayandasofficial120@gmail.com>
2026-02-06 10:52:26 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,19 +4,21 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
+
 db.connect((err) => {
-    if (err) {
-        console.log("Database Connection Failed");
-        console.log(err);
-    } else {
-        console.log("MySQL Connected");
-    }
+  if (err) {
+    console.log("❌ Database Connection Failed");
+    console.log(err);
+  } else {
+    console.log("✅ MySQL Connected");
+  }
 });
 
 export default db;
